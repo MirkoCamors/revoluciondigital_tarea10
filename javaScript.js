37422d03bae7ba2d2257ba4e4bcda918cd189e24
MirkoCamors <130615243+MirkoCamors@users.noreply.github.com>
2023-05-23 16:14:07 -0300

    Add files via upload

---
+++ javaScript.js
@@ -0,0 +1,14 @@
+var nombre = "Lionel"
+var apellido = "Messi"
+
+console.log(nombre +" "+ apellido)
+
+var numero1 = 10
+var numero2 = 39
+var suma = numero1 + numero2
+
+console.log(suma)
+
+var numero3 = suma + 400
+
+console.log(numero3)
